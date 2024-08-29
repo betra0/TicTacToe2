e1a15c17fdb192fa1a53cc934c3c0a9ee9be3b79
Danielhp <tolozadaniel5@gmail.com>
2024-08-29 19:16:34 -0400

    cree un menu

---
+++ web-client/src/pages/menuPage.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+function MenuPage(){
+
+
+
+
+    return(
+    <main className="bg-gray-900 w-screen h-screen  flex items-center flex-col text-white">
+        <h2 className=" mt-9 mb-12 text-center  text-5xl">Tic Tac Toe 2</h2>
+        <section className="mt-7 flex flex-1 w-full max-w-xxs flex-col text-center gap-7">
+        <Link className="bg-gray-800 w-full rounded-lg py-1 text-xl hover:bg-gray-700 transform hover:scale-110 duration-200 transition-transform active:scale-90" to="/">Local</Link>
+        <Link className="bg-gray-800 w-full rounded-lg py-1 text-xl hover:bg-gray-700 transform hover:scale-110 duration-200 transition-transform active:scale-90" to="/">Multiplayer</Link>
+        <Link className="bg-gray-800 w-full rounded-lg py-1 text-xl hover:bg-gray-700 transform hover:scale-110 duration-200 transition-transform active:scale-90" to="/">Settings</Link>
+        <Link className="bg-gray-800 w-full rounded-lg py-1 text-xl hover:bg-gray-700 transform hover:scale-110 duration-200 transition-transform active:scale-90" to="/">About</Link>
+
+            
+        </section>
+    </main>
+    )
+}
+    
+
+    
+export default MenuPage;
