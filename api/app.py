6c6b141c44a93fdb22382c2ee8f8cbf5c2d4501b
Danielhp <tolozadaniel5@gmail.com>
2024-09-16 15:20:48 -0300

    doker compose Con nginx y wsgi con una api Flask

--- api/app.py
+++ api/app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify({"message": "Hello, World!"})
+@app.route('/', methods=['GET'])
+def raiz():
+    return 'Hola Mundo'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ api/wsgi.py
@@ -0,0 +1,4 @@
+from app import app  # Asegúrate de ajustar esto según la estructura de tu aplicación
+
+if __name__ == "__main__":
+    app.run()
